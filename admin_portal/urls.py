709cd12c2667c180bbcfb51e1fe3c1bb6fecb92e
Sandeep <rayanuthalas@gmail.com>
2020-07-17 00:33:49 +0530

    URL Pk Added

--- admin_portal/urls.py
+++ admin_portal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import  views
 urlpatterns = [
     path('question/get', views.QuestionsListAPIView.as_view()),
-    path('question/update', views.QuestionUpdateAPIView.as_view()),
+    path('question/update/<pk>', views.QuestionUpdateAPIView.as_view()),
     path('question/create', views.QuestionCreateAPIView.as_view())
 ]

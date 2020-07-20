be92df083ba04c41b613984a59ef782a7be82cf5
Sandeep <rayanuthalas@gmail.com>
2020-07-20 15:07:32 +0530

    Pk update

--- participants/urls.py
+++ participants/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import  views
 urlpatterns = [
     path('dash',views.DashBoardListAPIView.as_view()),
-    path('code/view/<pk>',views.CodeRetrieveAPIView.as_view()),
+    path('code/view/<str:pk>',views.CodeRetrieveAPIView.as_view()),
     path('code/update',views.CodeCreateAPIView.as_view())
 ]

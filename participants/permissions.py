a9edefc00be5730ebfee8193605f17d49fbb0323
Sandeep <rayanuthalas@gmail.com>
2020-07-20 15:45:42 +0530

    Permissions uodate

--- participants/permissions.py
+++ participants/permissions.py
@@ -8,4 +8,4 @@ class IsObjOwner(BasePermission):
     message = "You don't have access to update this "
 
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and not request.user.is_disqualified and not request.user.is_blocked and obj.owner == request.user)
+        return bool(request.user and not request.user.is_disqualified and not request.user.is_blocked and obj.room_seat.participant == request.user)

d154771b01494fe019604eaf9c8de607e4b259d6
Sandeep <rayanuthalas@gmail.com>
2020-07-22 05:29:48 +0530

    Typo 😤

--- participants/urls.py
+++ participants/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/code/view/<pk>',views.CodeRetrieveAPIView.as_view()),
     path('admin/testcase/view/<pk>',views.TestCaseAPIView.as_view()),   
     path('submit/intiate/',views.IntiateSubmitQuestion.as_view()),
-    path('callback/<roomabsid:str>/<testid:str>',views.CallBackHandler.as_view()),
+    path('callback/<str:roomabsid>/<str:testid>',views.CallBackHandler.as_view()),
     path('submit',views.SubmitQuestion.as_view()),
 
 ]

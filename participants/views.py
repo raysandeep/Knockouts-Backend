2e5af59a02e141ab599a18eb46fe39e0c2f62141
Sandeep <rayanuthalas@gmail.com>
2020-07-24 00:31:31 +0530

    Testcase score

--- participants/views.py
+++ participants/views.py
@@ -437,8 +437,9 @@ class CheckSubmissions(APIView):
             
             # Testcases
             root_testcases = TestCaseHolder.objects.filter(question=question[0].id).filter(is_sample=False)
-            testcases = TestCaseSolutionLogger.objects.filter(room_solution=id)
+            testcases = TestCaseSolutionLogger.objects.filter(room_solution=id).filter(is_solved=True)
             testcases_solved = testcases.count()
+            print("TestCases Solved :",str(testcases_solved))
             total_testcases = root_testcases.count()
             #duration calculation
             duration = (seat[0].end_time -seat[0].start_time)
